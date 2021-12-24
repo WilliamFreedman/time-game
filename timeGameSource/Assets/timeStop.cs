86a385fea7b05b8a94782b80c187ab7819a916f3
WilliamFreedman <freedmanw@bxscience.edu>
2021-12-24 17:01:15 -0500

    Added timeStop, removed air control.

---
+++ timeGameSource/Assets/timeStop.cs
@@ -0,0 +1,40 @@
+
+using UnityEngine;
+
+public class timeStop : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    bool frozen = false;
+
+    public int gravValue = 3; // this is where we should edit gravity scale for the player, makes the scripting a lot easier
+
+    Vector3 currentVelocity = new Vector3(0,0,0);
+    // Update is called once per frame
+
+    void Start()
+    {
+        rb.gravityScale = gravValue;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && frozen)
+        {
+            rb.AddForce(currentVelocity*rb.mass,ForceMode2D.Impulse);
+            rb.gravityScale = gravValue;
+            frozen = false;
+        }
+
+        else if (Input.GetKeyDown("space") && !frozen)
+        {
+            currentVelocity = rb.velocity;
+            rb.velocity = new Vector3(0,0,0); 
+            rb.gravityScale = 0;
+            frozen = true;
+        }
+
+        
+    }
+
+}
