c5b07aaaf5d606092e3183ed80f6d2ecfc30a6d9
WilliamFreedman <freedmanw@bxscience.edu>
2021-12-24 15:43:21 -0500

    Added jumping and crouching, movement values need to be changed.

--- timeGameSource/Assets/playeMovement.cs
+++ timeGameSource/Assets/playeMovement.cs
@@ -11,15 +11,33 @@ public class playeMovement : MonoBehaviour
 
     float horizontalMove = 0f;
 
+    bool jump = false;
+
+    bool crouch = false;
+
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
