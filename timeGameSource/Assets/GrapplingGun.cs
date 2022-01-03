bfb456555a35d0fad94bdc4861ba7939feeff097
KareemDaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-01-03 18:00:56 -0500

    Revert "Works with moving platforms"
    This reverts commit 7febcae7650fa43446ca1b842c19649a1a056b93.

--- timeGameSource/Assets/GrapplingGun.cs
+++ timeGameSource/Assets/GrapplingGun.cs
@@ -28,11 +28,9 @@ public class GrapplingGun : MonoBehaviour
 
     [Header("Distance:")]
     [SerializeField] private bool hasMaxDistance = false;
-    [SerializeField] private float maxDistance = 20;
+    [SerializeField] private float maxDistnace = 20;
 
     public CharacterController2D controller;
-    private Rigidbody2D platform;
-    bool movingPlatform = false;
 
     private float gravValue;
     private enum LaunchType
@@ -64,11 +62,6 @@ public class GrapplingGun : MonoBehaviour
 
     }
 
-    private void FixedUpdate() {
-        if(Input.GetKey(KeyCode.Mouse0) && movingPlatform) {
-            grapplePoint+=.02f*platform.velocity;//multiplied by .02 to go from /sec to /frame
-        }
-    }
     private void Update()
     {
         if(!getFrozen.frozen) {
@@ -153,20 +146,12 @@ public class GrapplingGun : MonoBehaviour
             RaycastHit2D _hit = Physics2D.Raycast(firePoint.position, distanceVector.normalized);
             if (_hit.transform.gameObject.layer == grappableLayerNumber || grappleToAll)
             {
-                if (Vector2.Distance(_hit.point, firePoint.position) <= maxDistance || !hasMaxDistance)
+                if (Vector2.Distance(_hit.point, firePoint.position) <= maxDistnace || !hasMaxDistance)
                 {
                     grapplePoint = _hit.point;
                     grappleDistanceVector = grapplePoint - (Vector2)gunPivot.position;
                     grappleRope.enabled = true;
                 }
-            }
-            if(_hit.transform.gameObject.tag.Equals("pusher")) {
-                movingPlatform = true;
-                platform = _hit.rigidbody;
-            }
-            else {
-                movingPlatform = false;
-                platform = null;
             }
         }
     }
@@ -216,7 +201,7 @@ public class GrapplingGun : MonoBehaviour
         if (firePoint != null && hasMaxDistance)
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(firePoint.position, maxDistance);
+            Gizmos.DrawWireSphere(firePoint.position, maxDistnace);
         }
     }
 
