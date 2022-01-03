3212132ad10ae14ab92a761364f50c3a250e57d6
WilliamFreedman <freedmanw@bxscience.edu>
2022-01-02 21:12:40 -0500

    bouncer done
    need to figure out how to not have collisions affect it.

--- timeGameSource/Assets/Scripts/bouncer.cs
+++ timeGameSource/Assets/Scripts/bouncer.cs
@@ -8,17 +8,23 @@ public class bouncer : MonoBehaviour
     public Rigidbody2D self;
     public Collider2D leftBound;
     public Collider2D rightBound;
+    public float speed = 7;
+    
     // Start is called before the first frame update
     void Start()
     {
         self.isKinematic = false;
-        self.velocity = new Vector3(7,0,0);
+        Vector3 mag = new Vector3(7,0,0);
+        Vector3 targetVelocity = self.transform.right * speed;
+        self.velocity = new Vector2(targetVelocity.x,targetVelocity.y);
+        Debug.Log(self.is)
     }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("hi");
+        
         self.velocity *= -1;
     }
+
 }
