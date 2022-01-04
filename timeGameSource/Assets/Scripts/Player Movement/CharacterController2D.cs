256846fca5111e9a39d46f6bb34c44e2e11e9de5
KareemDaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-01-03 22:57:06 -0500

    Grapple WIP
    tired

--- timeGameSource/Assets/Scripts/Player Movement/CharacterController2D.cs
+++ timeGameSource/Assets/Scripts/Player Movement/CharacterController2D.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	private float m_JumpForce = 2000f;							// Amount of force added when the player jumps.
+	private float m_JumpForce = 2f;							// Amount of force added when the player jumps.
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	//[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
@@ -19,8 +19,12 @@ public class CharacterController2D : MonoBehaviour
 	public bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	public Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	public bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
+
+	public Sprite left;
+	public Sprite right;
+	public SpriteRenderer spriteRenderer;
 
 
 
@@ -141,18 +145,20 @@ public class CharacterController2D : MonoBehaviour
 			// Add a vertical force to the player.
 			m_Grounded = false;
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			Debug.Log(m_Rigidbody2D.gravityScale);
 		}
 	}
 
 
-	private void Flip()
+	public void Flip()
 	{
 		// Switch the way the player is labelled as facing.
 		m_FacingRight = !m_FacingRight;
 
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		//Flip Mario Around
+		if(m_FacingRight)
+			spriteRenderer.sprite = right;
+		else
+			spriteRenderer.sprite = left;
 	}
 }
